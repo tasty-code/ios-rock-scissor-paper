6d158883a841e123c516b75d87034a3ff9e28287
parkseyoon <issou06261@gmail.com>
2022-08-16 15:01:56 +0900

    [FEAT] 사용자 숫자 입력 함수 구현

--- rock-scissor-paper/rock-scissor-paper/main.swift
+++ rock-scissor-paper/rock-scissor-paper/main.swift
@@ -3,3 +3,9 @@ import Foundation
 func printMenu() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
 }
+
+func getUserNumber() -> Int {
+    let userNumber: Int? = Int(readLine() ?? "")
+    
+    return 0
+}
