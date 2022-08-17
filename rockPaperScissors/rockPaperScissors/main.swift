09c3e835f380ed2b041760ce789bcb1d6cd0fa6a
lcy <pgbenny0035@gmail.com>
2022-08-17 10:27:30 +0900

    feat: 게임 실행 함수 추가

--- rockPaperScissors/rockPaperScissors/main.swift
+++ rockPaperScissors/rockPaperScissors/main.swift
@@ -3,3 +3,9 @@ import Foundation
 func makeComputerValue() -> Int {
     return Int.random(in: 1...3)
 }
+
+func startGame() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: " ")
+}
+
+startGame()
