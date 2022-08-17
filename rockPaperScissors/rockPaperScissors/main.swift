655ad791cddb477d912ee2fa6c4d9d90023dedd9
lcy <pgbenny0035@gmail.com>
2022-08-17 10:40:26 +0900

    feat: 유저 패 검사 함수 추가

--- rockPaperScissors/rockPaperScissors/main.swift
+++ rockPaperScissors/rockPaperScissors/main.swift
@@ -8,12 +8,30 @@ func startGame() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: " ")
 }
 
+func checkUserValue(_ userValue: Int?) -> Int? {
+    switch userValue {
+    case 0:
+        return nil
+    case 1, 2, 3:
+        return userValue
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+    
+    return nil
+}
 func inputUserValue() {
     let userValue = readLine() ?? "nil"
+    var integerUserValue: Int? = 0
+    
     
     if let value = Int(userValue) {
-        print(value)
+        integerUserValue = value
     }
+    
+    integerUserValue = checkUserValue(integerUserValue)
+    
+    print(integerUserValue)
 }
 
 inputUserValue()
