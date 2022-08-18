6a447258f16e07dac97678e56278a89635f8a36f
박도원 <ddowon@bagdowon-ui-MacBookAir.local>
2022-08-18 10:42:55 +0900

    refactor: 네이밍 수정

--- rock-scissor-paper/rock-scissor-paper/main.swift
+++ rock-scissor-paper/rock-scissor-paper/main.swift
@@ -19,28 +19,28 @@ func createRandomNumber() -> Int {
     return computerRandomNumber
 }
 
-func analysisUserInput(userInput: Int) {
+func analyzeUserInput(userNumber: Int) {
     let computerRandomNumber = createRandomNumber()
     
-    if userInput == computerRandomNumber {
+    if userNumber == computerRandomNumber {
         print("비겼습니다!\n")
-    } else if computerRandomNumber + 1 == userInput || computerRandomNumber == 3 && userInput == 1 {
+    } else if computerRandomNumber + 1 == userNumber || computerRandomNumber == 3 && userNumber == 1 {
         print("이겼습니다!\n")
         isDone = true
-    } else if userInput == 0 {
+    } else if userNumber == 0 {
         isDone = true
-    } else if computerRandomNumber - 1 == userInput || computerRandomNumber == 1 && userInput == 3 {
+    } else if computerRandomNumber - 1 == userNumber || computerRandomNumber == 1 && userNumber == 3 {
         print("졌습니다!\n")
         isDone = true
     } else {
         print("잘못된 입력입니다. 다시 시도해 주세요.\n")
-        analysisUserInput(userInput: getMenuInput())
+        analyzeUserInput(userNumber: getMenuInput())
     }
 }
 
 func startGame() {
     while !isDone {
-        analysisUserInput(userInput: getMenuInput())
+        analyzeUserInput(userNumber: getMenuInput())
     }
 }
 
