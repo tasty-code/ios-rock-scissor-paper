8aa8015f61b22c96d0d39b8c4c2899aceac0f61f
parkseyoon <issou06261@gmail.com>
2022-08-18 10:03:46 +0900

    fix : 개행 방법 수정

--- rock-scissor-paper/rock-scissor-paper/main.swift
+++ rock-scissor-paper/rock-scissor-paper/main.swift
@@ -11,7 +11,6 @@ func printMenu() {
     
     let computerNumber: Int = Int.random(in: cardNumbersRange)
     judgeNumber(computerNumber: computerNumber, userNumber: userNumber)
-    print()
     printMenu()
 }
 
@@ -40,11 +39,11 @@ func isValidNumber(userNumber: Int) -> Bool {
 
 func judgeNumber(computerNumber: Int, userNumber: Int) -> Void {
     if (computerNumber == 1 && userNumber == 3) || userNumber < computerNumber {
-        print("졌습니다!")
+        print("졌습니다!\n")
     } else if computerNumber == userNumber {
-        print("비겼습니다!")
+        print("비겼습니다!\n")
     } else {
-        print("이겼습니다!")
+        print("이겼습니다!\n")
     }
 }
 
