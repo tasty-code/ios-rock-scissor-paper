8e23b803bef10101a5d6ad38ed364b859d5d7189
parkseyoon <issou06261@gmail.com>
2022-08-19 14:29:40 +0900

    [ADD] 묵찌빠 시작 함수, 묵찌빠 판단 함수, 이긴 플레이어 받아오는 함수 생성

--- rock-scissor-paper/rock-scissor-paper/main.swift
+++ rock-scissor-paper/rock-scissor-paper/main.swift
@@ -1,13 +1,12 @@
 import Foundation
 
 let cardNumbersRange = 1...3
-let player: [String: String] = ["computer": "컴퓨터", "user": "사용자"]
 
 func startGame() {
     printMenu()
     let computerNumber: Int = makeComputerNumber()
     let userNumber: Int = manageUserNumber()
-    if isValidNumber(userNumber: userNumber) == false {
+    if isValidNumber(type: "normal",userNumber: userNumber) == false {
         return
     }
     judgeNumber(computerNumber: computerNumber, userNumber: userNumber)
@@ -17,8 +16,20 @@ func printMenu() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
 }
 
+func startMukJiPa(winPlayer: String) {
+    printGame(winPlayer: winPlayer)
+    let computerNumber: Int = makeComputerNumber()
+    let userNumber: Int = manageUserNumber()
+    
+    if isValidNumber(type: "MukJiPa", userNumber: userNumber) == false {
+        return
+    }
+    
+    judgeMukJiPaNumber(winPlayer: winPlayer, computerNumber: computerNumber, userNumber: userNumber)
+}
+
 func printGame(winPlayer: String) {
-    print("[\(player[winPlayer])턴] 묵(1), 찌(2), 빠(3)! <종료 : 0>", terminator: "")
+    print("[\(getWinPlayerName(winPlayer: winPlayer)) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
 }
 
 func makeComputerNumber() -> Int {
@@ -39,7 +50,7 @@ func getUserNumber() -> Int {
     return -1
 }
 
-func isValidNumber(userNumber: Int) -> Bool {
+func isValidNumber(type: String, userNumber: Int) -> Bool {
     var isValidBool: Bool = true
     switch userNumber {
     case 0:
@@ -47,16 +58,21 @@ func isValidNumber(userNumber: Int) -> Bool {
     case cardNumbersRange:
         break
     default:
-        invalidInputHandling()
+        invalidInputHandling(type: type)
         isValidBool = false
     }
     
     return isValidBool
 }
 
-func invalidInputHandling() {
+func invalidInputHandling(type: String, winPlayer: String = "") {
     print("잘못된 입력입니다. 다시 시도해주세요.\n")
     startGame()
+    if type == "normal" {
+        startGame()
+    } else {
+        startMukJiPa(winPlayer: <#T##String#>)
+    }
 }
 
 func judgeNumber(computerNumber: Int, userNumber: Int) -> Void {
@@ -65,13 +81,38 @@ func judgeNumber(computerNumber: Int, userNumber: Int) -> Void {
     switch computerAndUserNumber {
     case [1: 2], [2: 3], [3: 1]:
         print("이겼습니다!\n")
+        startMukJiPa(winPlayer: "user")
     case [1: 3], [2: 1], [3: 2]:
         print("졌습니다!\n")
+        startMukJiPa(winPlayer: "computer")
     default:
         print("비겼습니다!\n")
     }
     
     startGame()
+}
+
+func judgeMukJiPaNumber(winPlayer: String, computerNumber: Int, userNumber: Int) -> Void {
+    let computerAndUserNumber: [Int: Int] = [computerNumber: userNumber]
+    
+    switch computerAndUserNumber {
+    case [1: 2], [2: 3], [3: 1]:
+        print("")
+        startMukJiPa(winPlayer: winPlayer)
+    case [1: 3], [2: 1], [3: 2]:
+        
+        startMukJiPa(winPlayer: winPlayer)
+    default:
+        print("\(getWinPlayerName(winPlayer: winPlayer))의 승리!")
+    }
+}
+
+func getWinPlayerName(winPlayer: String) -> String {
+    let player: [String: String] = ["computer": "컴퓨터", "user": "사용자"]
+    if let winPlayerName = player[winPlayer] {
+        return winPlayerName
+    }
+    return "-"
 }
 
 startGame()
