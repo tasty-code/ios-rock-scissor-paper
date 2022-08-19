c73334c205da8ef345bd8d069eb27e5a24aec425
박도원 <ddowon@bagdowon-ui-MacBookAir.local>
2022-08-19 12:12:10 +0900

    refactor: analyzeUserIuput함수 제어문 변경

--- rock-scissor-paper/rock-scissor-paper/main.swift
+++ rock-scissor-paper/rock-scissor-paper/main.swift
@@ -3,12 +3,12 @@ import Foundation
 var isDone: Bool = false
 
 func showMenu() {
-     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
 }
 
 func getMenuInput() -> Int {
     guard let userInput = readLine(), let input = Int(userInput) else {
-        return Int()
+        return 0
     }
     return input
 }
@@ -16,7 +16,7 @@ func getMenuInput() -> Int {
 func createRandomNumber() -> Int {
     enum Rockscissorpaper: Int {
         case scissor = 1, rock, paper
-    }       
+    }
     let computerRandomNumber = Int.random(in: Rockscissorpaper.scissor.rawValue...Rockscissorpaper.paper.rawValue)
     
     return computerRandomNumber
@@ -24,19 +24,23 @@ func createRandomNumber() -> Int {
 
 func analyzeUserInput(userNumber: Int) {
     let computerRandomNumber = createRandomNumber()
+    let comparisonOfNumber = userNumber - computerRandomNumber
     
-    if userNumber == computerRandomNumber {
+    switch comparisonOfNumber {
+    case 0:
+        print(computerRandomNumber)
         print("비겼습니다!\n")
-    } else if computerRandomNumber + 1 == userNumber || computerRandomNumber == 3 && userNumber == 1 {
+    case 1 where (-2 != 0):
+        print(computerRandomNumber)
         print("이겼습니다!\n")
         isDone = true
-    } else if userNumber == 0 {
-        isDone = true
-    } else if computerRandomNumber - 1 == userNumber || computerRandomNumber == 1 && userNumber == 3 {
+    case -1 where (2 != 0):
+        print(computerRandomNumber)
         print("졌습니다!\n")
         isDone = true
-    } else {
+    default:
         print("잘못된 입력입니다. 다시 시도해 주세요.\n")
+        showMenu()
         analyzeUserInput(userNumber: getMenuInput())
     }
 }
