59ff396c70f2a86e4e86612ecea8ab33f1efd406
moonsoungjin <seongjinmun74@gmail.com>
2022-09-13 22:53:04 +0900

    사용자가 보자기를 냈을때 결과

--- Rock/Rock/main.swift
+++ Rock/Rock/main.swift
@@ -11,3 +11,15 @@ func rock(_ computerNumber: Int) {
         print("졌습니다")
     }
 }
+
+func paper(_ computerNumber: Int) {
+    if computerNumber == 1 {
+        print("졌습니다")
+    }
+    else if computerNumber == 2 {
+        print("이겼습니다")
+    }
+    else if computerNumber == 3 {
+        print("비겼습니다")
+    }
+}
