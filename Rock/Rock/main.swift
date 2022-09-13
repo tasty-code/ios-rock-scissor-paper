4d5dc36b54db3ee017a61640cf2931459adc33e0
moonsoungjin <seongjinmun74@gmail.com>
2022-09-13 22:52:23 +0900

    사용자가 주먹을 냈을때 결과

---
+++ Rock/Rock/main.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func rock(_ computerNumber: Int) {
+    if computerNumber == 1 {
+        print("이겼습니다")
+    }
+    else if computerNumber == 2 {
+        print("비겼습니다")
+    }
+    else if computerNumber == 3 {
+        print("졌습니다")
+    }
+}
