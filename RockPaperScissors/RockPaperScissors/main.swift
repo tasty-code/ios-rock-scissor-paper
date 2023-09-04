cd95be53aca43791e04d8b8556856212c9cc48eb
kimjunsung <assd1108@naver.com>
2023-09-04 16:30:21 +0900

    feat: 가위바위보 상성관계 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,57 @@
 
 import Foundation
 
-print("Hello, World!")
+while true {
+    print("가위(1), 바위(2), 보(3)! <종료: 0> : ")
+    var str = readLine() ?? "error"
 
+    guard let num = Int(str), (0...3).contains(num) else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        continue
+    }
+    
+    switch num {
+    case 0:
+        break
+    case 1:
+        break
+    case 2:
+        break
+    case 3:
+        break
+    default:
+        break
+    }
+}
+
+func battle(player: Int) {
+    guard let computer = (1...3).randomElement() else {
+        print("오류가 발생했습니다.")
+        return
+    }
+    
+    if player == 1 && computer == 1 {
+        print("비겼습니다.")
+    } else if player == 1 && computer == 2 {
+        print("졌습니다.")
+    } else {
+        print("이겼습니다.")
+    }
+    
+    
+    if player == 2 && computer == 1 {
+        print("이겼습니다.")
+    } else if player == 2 && computer == 2 {
+        print("비겼습니다.")
+    } else {
+        print("졌습니다.")
+    }
+    
+    if player == 3 && computer == 1 {
+        print("졌습니다.")
+    } else if player == 3 && computer == 2 {
+        print("이겼습니다.")
+    } else {
+        print("비겼습니다.")
+    }
+}
