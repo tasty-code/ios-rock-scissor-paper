8ed45fe153908bec64b54487dd83633829ee5757
SimJaeHyeok <jaehyeoking@gmail.com>
2023-09-05 17:52:05 +0900

    refactor: 가위바위보 상성관계 리팩토링

---
+++ RockPaperScissors/RockPaperScissors.swift
@@ -0,0 +1,27 @@
+//
+//  RockScissorsPaper.swift
+//  RockPaperScissors
+//
+//  Created by JaeHyeok Sim on 2023/09/05.
+//
+
+import Foundation
+
+enum RockPaperScissors: Int, Comparable {
+    case scissors = 1
+    case rock = 2
+    case paper = 3
+    
+    static func < (lhs: RockPaperScissors, rhs: RockPaperScissors) -> Bool {
+        if lhs == .paper && rhs == .scissors {
+            return true
+        }
+        if lhs == .rock && rhs == .paper {
+            return true
+        }
+        if lhs == .scissors && rhs == .rock {
+            return true
+        }
+        return false
+    }
+}
--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -5,58 +5,39 @@
 // 
 
 import Foundation
+var isContinue = true
 
-while true {
-    print("가위(1), 바위(2), 보(3)! <종료: 0> : ")
-    var str = readLine() ?? "error"
+while isContinue {
+    print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
+    let str = readLine() ?? "error"
 
     guard let num = Int(str), (0...3).contains(num) else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
         continue
     }
-    
-    switch num {
-    case 0:
-        break
-    case 1:
-        break
-    case 2:
-        break
-    case 3:
-        break
-    default:
-        break
-    }
+    isContinue = battle(player: num)
 }
 
-func battle(player: Int) {
+
+func battle(player: Int) -> Bool {
     guard let computer = (1...3).randomElement() else {
         print("오류가 발생했습니다.")
-        return
+        return false
     }
     
-    if player == 1 && computer == 1 {
-        print("비겼습니다.")
-    } else if player == 1 && computer == 2 {
-        print("졌습니다.")
-    } else {
-        print("이겼습니다.")
+    guard let playerHand = RockPaperScissors(rawValue: player), let computerHand = RockPaperScissors(rawValue: computer) else {
+        print("오류가 발생했습니다.")
+        return false
     }
     
-    
-    if player == 2 && computer == 1 {
-        print("이겼습니다.")
-    } else if player == 2 && computer == 2 {
-        print("비겼습니다.")
-    } else {
-        print("졌습니다.")
-    }
-    
-    if player == 3 && computer == 1 {
-        print("졌습니다.")
-    } else if player == 3 && computer == 2 {
-        print("이겼습니다.")
-    } else {
-        print("비겼습니다.")
+    if playerHand == computerHand {
+        print("비겼습니다!")
+        return true
+    }else if computerHand < playerHand {
+        print("이겼습니다!")
+        return false
+    }else{
+        print("졌습니다!")
+        return false
     }
 }
