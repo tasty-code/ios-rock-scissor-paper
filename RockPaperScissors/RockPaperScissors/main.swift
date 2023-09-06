361d340e45645b0ef56b944e255215926bc219a6
snowy-summer <chsb1221@naver.com>
2023-09-06 09:44:14 +0900

    chore: 기존 2개의 guard문을 1개로 병합

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -38,14 +38,9 @@ func winLoseResult(_ input : Int) ->  Bool
 while true {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
     
-    guard let input = readLine() else
+    guard let input = readLine(), let input = Int(input) else
     {
        break
-    }
-        
-    guard let input = Int(input) else
-    {
-        break
     }
         
     switch input
