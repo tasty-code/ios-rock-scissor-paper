af140be454e193f64931fb2e8871ac549871a307
bamsak <qjatjrdl963@naver.com>
2023-09-06 10:44:20 +0900

    [Creat, Feat] 프로젝트 초기 생성 및 InterfaceScript모델 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,42 @@
 
 import Foundation
 
-print("Hello, World!")
+enum InterfaceScript {
+    
+    enum Menu: CustomStringConvertible {
+        case userInput, exitProgram
+        
+        var description: String {
+            switch self {
+            case .userInput: return "가위(1), 바위(2), 보(3)! <종료 : 0> : "
+            case .exitProgram: return "게임 종료"
+            }
+        }
+    }
+    
+    enum Failure: Error, CustomDebugStringConvertible {
+        case wrongInput
+        
+        var debugDescription: String {
+            switch self {
+            case .wrongInput: return "잘못된 입력입니다. 다시 시도해주세요."
+            }
+        }
+        
+    }
+    
+    enum GameResult: CustomStringConvertible {
+        case win, lose, draw
+        
+        var description: String {
+            switch self {
+            case .win: return "이겼습니다!"
+            case .lose: return "졌습니다!"
+            case .draw: return "비겼습니다!"
+            }
+        }
+        
+    }
+    
+}
 
