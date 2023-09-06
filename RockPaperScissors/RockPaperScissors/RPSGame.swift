4490d7f8115114bce823af7c437cf0d28a55abcb
JinUng41 <ingjwjw@naver.com>
2023-09-06 11:12:20 +0900

    feat: add game exit case and generateRandomInteger

--- RockPaperScissors/RockPaperScissors/RPSGame.swift
+++ RockPaperScissors/RockPaperScissors/RPSGame.swift
@@ -7,20 +7,25 @@
 
 import Foundation
 
+
 class RPSGame {
-    
     
     func run() {
         while true {
             printMenu()
             
             let userChoice = getUserChoice()
+            let botChoice = generateRandomInteger()
+            
+            if userChoice == 0 {
+                print("게임 종료")
+                break
+            }
             
         }
     }
     
     private func printMenu() {
-        
         print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
     }
     
@@ -29,7 +34,11 @@ class RPSGame {
             print("잘못된 입력입니다. 다시 시도해주세요.")
             return getUserChoice()
         }
-        
+
         return number
+    }
+    
+    private func generateRandomInteger() -> Int {
+        return Int.random(in: 1...3)
     }
 }
