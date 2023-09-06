8e959b6d5e7772d3106afba7d5983fea60af6c71
KimRoks <rudfhr3161@naver.com>
2023-09-06 13:27:25 +0900

    refactor: 변수 네이밍 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -2,13 +2,12 @@
 //  RockPaperScissors - main.swift
 //  Created by tacocat.
 //  Copyright © tastycode. All rights reserved.
-// 
-
+//
 var userSelect: String?
 var computerSelect: String
-var status: Bool = true
+var isRunning: Bool = true
 
-while status {
+while isRunning {
     print("가위(1), 바위(2), 보(3)! <종료 : 0 > : ", terminator: "")
     userSelect = readLine()
     computerSelect = String(Int.random(in: 1...3))
@@ -20,7 +19,7 @@ while status {
     
     switch select {
     case "0":
-        status = false
+        isRunning = false
     case "1", "2", "3":
         if select == computerSelect {
             print("비겼습니다")
@@ -29,10 +28,10 @@ while status {
             select == "2" && computerSelect == "1" ||
             select == "3" && computerSelect == "2" {
             print("이겼습니다")
-            status = false
+            isRunning = false
         } else {
             print("졌습니다")
-            status = false
+            isRunning = false
         }
     default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
