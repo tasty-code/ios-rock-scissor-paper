ca859914a5e84704198a09bfc1d45dfe30fc24ee
KimRoks <rudfhr3161@naver.com>
2023-09-06 14:01:14 +0900

    refactor: 불필요한 전역변수 및 중복코드 제거

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -3,19 +3,13 @@
 //  Created by tacocat.
 //  Copyright © tastycode. All rights reserved.
 //
-var userSelect: String?
-var computerSelect: String
+//var computerSelect: String
 var isRunning: Bool = true
 
 while isRunning {
     print("가위(1), 바위(2), 보(3)! <종료 : 0 > : ", terminator: "")
-    userSelect = readLine()
-    computerSelect = String(Int.random(in: 1...3))
-    
-    guard let select = userSelect else {
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-        break
-    }
+    guard let select = readLine() else { break }
+    let computerSelect = String(Int.random(in: 1...3))
     
     switch select {
     case "0":
@@ -25,8 +19,8 @@ while isRunning {
             print("비겼습니다")
             break
         } else if select == "1" && computerSelect == "3" ||
-            select == "2" && computerSelect == "1" ||
-            select == "3" && computerSelect == "2" {
+                    select == "2" && computerSelect == "1" ||
+                    select == "3" && computerSelect == "2" {
             print("이겼습니다")
             isRunning = false
         } else {
