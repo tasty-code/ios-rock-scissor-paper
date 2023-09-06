e53d1056b224134b6354111a0df3b0c8063c2186
COMDORI <02145s1@gmail.com>
2023-09-06 10:26:06 +0900

    Feat Hand enum 추가, 시작 기능 추가

--- RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
@@ -8,4 +8,13 @@
 import Foundation
 
 final class RockPaperScissors {
+    enum Hand : String, CaseIterable {
+        case scissors = "1"
+        case rock = "2"
+        case paper = "3"
+    }
+    
+    func start() {
+        
+    }
 }
