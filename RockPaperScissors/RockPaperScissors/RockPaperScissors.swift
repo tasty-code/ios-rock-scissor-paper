e645f603906ac659f902cc6b2a23cd2379bfc082
COMDORI <02145s1@gmail.com>
2023-09-07 16:45:59 +0900

    Refactor 재귀함수 사용, 기능 단위 함수 분리
    - while 반복문 삭제
    - 유저 입력 함수 분리
    - 결과 출력 함수 분리

--- RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
@@ -18,26 +18,57 @@ final class RockPaperScissors {
         case paper = 3
     }
     
+    enum GameResult {
+        case win
+        case lose
+        case draw
+    }
+    
+    // MARK: - Property
+    
+    private var gameResult: GameResult = .draw
+    private var userInput = 1
+    private var userChoice : RpsChoice = .scissors
+    
     // MARK: - Start
     
     func start() {
-        while true {
-            print(GuideMessage.rpsChoiceMenu, terminator: "")
-            guard let userInput = Int(readLine() ?? "0"), let userChoice = RpsChoice(rawValue: userInput) else {
-                print(GuideMessage.inputError)
-                return
-            }
-            
-            if userInput == 0 {
-                return print(GuideMessage.exit)
-            }
-            
-            let computerChoice = RpsChoice(rawValue: Int.random(in: 1...3))
-            switch (userChoice, computerChoice) {
-            case (.scissors, .paper), (.rock, .scissors), (.paper, .rock): return print(GuideMessage.win)
-            case (.paper, .scissors), (.scissors, .rock), (.rock, .paper): return print(GuideMessage.lose)
-            default: print(GuideMessage.draw)
-            }
+        print(GuideMessage.rpsChoiceMenu, terminator: "")
+        
+        enterUserChoice {
+            start()
+        }
+        
+        if userInput == 0 {
+            return print(GuideMessage.exit)
+        }
+        
+        guard let computerChoice = RpsChoice(rawValue: Int.random(in: 1...3)) else { return }
+        gameResult = checkResult(of: userChoice, with: computerChoice) { result in
+            print(result)
+        }
+    }
+    private func enterUserChoice(whenMistake: () -> ()) {
+        guard let userInput = Int(readLine() ?? "0"),
+              let userChoice = RpsChoice(rawValue: userInput) else {
+            print(GuideMessage.inputError)
+            return
+        }
+        self.userInput = userInput
+        self.userChoice = userChoice
+    }
+    
+    private func checkResult(of userChoice: RpsChoice, with computerChoice: RpsChoice, message: (String) -> ()) -> GameResult {
+        switch (userChoice, computerChoice) {
+        case (.scissors, .paper), (.rock, .scissors), (.paper, .rock):
+            message(GuideMessage.win)
+            return .win
+        case (.paper, .scissors), (.scissors, .rock), (.rock, .paper):
+            message(GuideMessage.lose)
+            return .lose
+        default:
+            message(GuideMessage.draw)
+            return .draw
         }
     }
 }
