69601731b88b90373263eab48823d1655196e60b
COMDORI <02145s1@gmail.com>
2023-09-07 16:10:30 +0900

    Refactor 스위치문 사용, 변수명 수정

--- RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
@@ -11,10 +11,11 @@ final class RockPaperScissors {
     
     // MARK: - Enum
     
-    enum Hand : String, CaseIterable {
-        case scissors = "1"
-        case rock = "2"
-        case paper = "3"
+    enum RpsChoice : Int {
+        case exit = 0
+        case scissors = 1
+        case rock = 2
+        case paper = 3
     }
     
     // MARK: - Start
@@ -22,27 +23,20 @@ final class RockPaperScissors {
     func start() {
         while true {
             print(GuideMessage.rpsChoiceMenu, terminator: "")
-            guard let userInput = readLine() else { return }
+            guard let userInput = Int(readLine() ?? "0"), let userChoice = RpsChoice(rawValue: userInput) else {
+                print(GuideMessage.inputError)
+                return
+            }
             
-            if userInput == "0" {
+            if userInput == 0 {
                 return print(GuideMessage.exit)
             }
             
-            guard let userHand = Hand(rawValue: userInput) else {
-                print(GuideMessage.inputError)
-                continue
-            }
-            
-            let aiHand = Hand.allCases.randomElement()
-            
-            if userHand == aiHand {
-                print(GuideMessage.draw)
-            } else if (userHand == .scissors && aiHand == .paper) ||
-                      (userHand == .rock && aiHand == .scissors) ||
-                      (userHand == .paper && aiHand == .rock) {
-                return print(GuideMessage.win)
-            } else {
-                return print(GuideMessage.lose)
+            let computerChoice = RpsChoice(rawValue: Int.random(in: 1...3))
+            switch (userChoice, computerChoice) {
+            case (.scissors, .paper), (.rock, .scissors), (.paper, .rock): return print(GuideMessage.win)
+            case (.paper, .scissors), (.scissors, .rock), (.rock, .paper): return print(GuideMessage.lose)
+            default: print(GuideMessage.draw)
             }
         }
     }
