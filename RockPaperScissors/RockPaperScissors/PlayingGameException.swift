6808d274885c38edc18ca9a6bb5af8760ceedec2
SimJaeHyeok <jaehyeoking@gmail.com>
2023-09-07 09:58:12 +0900

    style: 변수 및 열거형 이름 변경

--- RockPaperScissors/RockPaperScissors/PlayingGameException.swift
+++ RockPaperScissors/RockPaperScissors/PlayingGameException.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-enum RPCError: Error {
+enum PlayingGameException: Error {
     case invalidInputError
 }
--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -9,36 +9,35 @@ import Foundation
 var isContinue = true
 while isContinue {
     print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
-    let str = readLine() ?? "error"
-    guard let num = Int(str), (0...3).contains(num) else {
+    let playerInput = readLine() ?? "error"
+    guard let playerHandType = Int(playerInput), (0...3).contains(playerHandType) else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
         continue
     }
     
     do {
-        isContinue = try battle(player: num)
+        isContinue = try battle(player: playerHandType)
     } catch {
         print("잘못된 입력입니다. 다시 시도해주세요.")
     }
 }
 
-
 func battle(player: Int) throws -> Bool {
-    guard let computer = (1...3).randomElement() else {
-        throw RPCError.invalidInputError
+    guard let computerHandType = (1...3).randomElement() else {
+        throw PlayingGameException.invalidInputError
     }
     
-    guard let playerHand = RockPaperScissors(rawValue: player), let computerHand = RockPaperScissors(rawValue: computer) else {
-        throw RPCError.invalidInputError
+    guard let playerHandShape = RockPaperScissors(rawValue: player), let computerHandShape = RockPaperScissors(rawValue: computerHandType) else {
+        throw PlayingGameException.invalidInputError
     }
     
-    if playerHand == computerHand {
+    if playerHandShape == computerHandShape {
         print("비겼습니다!")
         return true
-    }else if computerHand < playerHand {
+    } else if computerHandShape < playerHandShape {
         print("이겼습니다!")
         return false
-    }else{
+    } else {
         print("졌습니다!")
         return false
     }
