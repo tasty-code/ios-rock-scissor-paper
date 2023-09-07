1fd8207e294d3fd81d90b46e1b82bb8299f81b5d
KimRoks <rudfhr3161@naver.com>
2023-09-07 16:01:39 +0900

    rarla turn

--- RockPaperScissors/RockPaperScissors/Utillity.swift
+++ RockPaperScissors/RockPaperScissors/Utillity.swift
@@ -20,29 +20,31 @@ func getRPSResult(userSelcet: String, computerSelect: String) {
         print("이겼습니다")
         turn = "user"
         game()
-        //        isRunning = false
+        
     } else {
         print("졌습니다")
         turn = "computer"
-        //        isRunning = false
+        
         game()
     }
 }
 
-func game2Start(userSelcet: String, computerSelect: String) {
-//  var ruf =
-  if userSelcet == computerSelect {
-    print("\(turn)의 승리!")
-    isRunning = false
-  } else if userSelcet == scissors && computerSelect == paper ||
-    userSelcet == rock && computerSelect == scissors ||
-    userSelcet == paper && computerSelect == rock {
-//    ruf = win lose
-  } else {
-    turn = "computer"
-//    ruf = win lose
-  }
-  game()
+func game2Start(userSelcet: String, computerSelect: String) -> String {
+    var result: String = ""
+    if userSelcet == computerSelect {
+        print("\(turn)의 승리!")
+        isRunning = false
+    } else if userSelcet == scissors && computerSelect == paper ||
+                userSelcet == rock && computerSelect == scissors ||
+                userSelcet == paper && computerSelect == rock {
+        result = "win"
+    } else {
+        turn = "computer"
+        result = "lose"
+    }
+    
+    game()
+    return result
 }
 
 
@@ -52,13 +54,15 @@ func game(){
     print("[\(turn)] 묵(1), 찌(2), 빠(3)!, <종료: 0> : ",terminator: "")
     let computerSelect = getRandomNum()
     guard let userSelcet = readLine() else { return }
-//    ruf game2Start(userSelcet: userSelcet, computerSelect: computerSelect)
     
-//  if 결과값 처리
-//    if turn == "user" {
-//        game2Start(userSelcet: userSelcet, computerSelect: computerSelect)
-//    } else {
-//
-//    }
+   let result = game2Start(userSelcet: userSelcet, computerSelect: computerSelect)
+    if result == "win" {
+        game()
+        
+    } else {
+        turn = turn == "user" ? "computer" : "user"
+        game()
+    }
+    
     
 }
