040818b6b2798b4778552b672d29e88b49b41223
mireu79 <https://github.com/mireu79>
2023-11-20 17:37:59 +0900

    feat: checkingOpponentHand 함수를 통해 상대방의 임의의 패 설정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,21 +6,20 @@
 
 import Foundation
 
+let opponentHands: [Int] = [1, 2, 3]
+
 func choiceRockScissorsPaper() {
     let isRunning: Bool = true
     
     while isRunning {
         print("가위(1), 바위(2), 보(3)! <종료: 0> :", terminator: " ")
         guard let input = readLine() else { return }
+        guard let input2 = Int(input) else { return }
         
-        switch input {
-        case "0":
+        switch input2 {
+        case 0:
             return
-        case "1":
-            return
-        case "2":
-            return
-        case "3":
+        case 1...3:
             return
         default:
             print("잘못된 입력입니다. 다시 시도해주세요.")
@@ -29,5 +28,10 @@ func choiceRockScissorsPaper() {
     }
 }
 
-choiceRockScissorsPaper()
+func checkingOpponentHand() -> Int {
+    guard let opponentHand = opponentHands.randomElement() else { return 0 }
+    
+    return opponentHand
+}
+
 
