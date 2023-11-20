2d6a31490ef2564661232e4dca18f5261450da05
Changhyun Kang <changhyun.kyle@gmail.com>
2023-11-20 16:35:46 +0900

    feat: 가위바위보 게임 최초 실행 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,28 @@
 
 import Foundation
 
-print("Hello, World!")
+func choiceRockScissorsPaper() {
+    let isRunning: Bool = true
+    
+    while isRunning {
+        print("가위(1), 바위(2), 보(3)! <종료: 0> :", terminator: " ")
+        guard let input = readLine() else { return }
+        
+        switch input {
+        case "0":
+            return
+        case "1":
+            return
+        case "2":
+            return
+        case "3":
+            return
+        default:
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+         continue
+        }
+    }
+}
+
+choiceRockScissorsPaper()
 
