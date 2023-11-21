bfc377116e42f415d5ce899765ea7818ce4dcdcd
WONGI HONG <dt95@naver.com>
2023-11-21 10:32:52 +0900

    feat: 열거형 case 생성

---
+++ RockPaperScissors/RockPaperScissors/Case.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Case: String {
+    case win = "이겼습니다!"
+    case lose = "졌습니다!"
+    case draw = "비겼습니다!"
+    case falseValue = "잘못된 입력입니다. 다시 시도해주세요."
+    case exit = "게임 종료"
+}
