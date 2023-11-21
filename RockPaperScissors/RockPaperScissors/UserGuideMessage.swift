cabb8d02d632e9cc261174869afd26ffd917f232
eunjuchoi <eunjuzzoo@naver.com>
2023-11-21 17:23:15 +0900

    feat: UserGuideMessage enum 정의 (#1)

---
+++ RockPaperScissors/RockPaperScissors/UserGuideMessage.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  RockPaperScissors
+//
+//  Created by EUNJU on 2023/11/21.
+//
+
+import Foundation
+
+enum UserGuideMessage {
+    case `default`
+    case win
+    case lose
+    case draw
+    case inputError
+}
