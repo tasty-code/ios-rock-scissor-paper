74d3f04a29b744668dadf1a31d2e6c0c98bed044
eunjuchoi <eunjuzzoo@naver.com>
2023-11-21 17:24:40 +0900

    feat: IOManager 클래스 추가 (#1)
    - 출력 메시지 보여주는 메서드 추가

---
+++ RockPaperScissors/RockPaperScissors/IOManager.swift
@@ -0,0 +1,27 @@
+//
+//  IOManager.swift
+//  RockPaperScissors
+//
+//  Created by EUNJU on 2023/11/21.
+//
+
+import Foundation
+
+final class IOManager {
+    
+    func showMessage(_ option: UserGuideMessage) {
+        switch option {
+            
+        case .default:
+            print("가위(1), 바위(2), 보(3)! <종료 : 0> : ")
+        case .win:
+            print("이겼습니다!")
+        case .lose:
+            print("졌습니다!")
+        case .draw:
+            print("비겼습니다!")
+        case .inputError:
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+        }
+    }
+}
