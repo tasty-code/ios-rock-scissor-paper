808efb7391cb933c97e7c75faf9e9a0b39e7aeff
eunjuchoi <eunjuzzoo@naver.com>
2023-11-21 17:27:05 +0900

    feat: RockPaperScissorsManager 클래스 정의 (#1)

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsManager.swift
@@ -0,0 +1,16 @@
+//
+//  RockPaperScissorsManager.swift
+//  RockPaperScissors
+//
+//  Created by EUNJU on 2023/11/21.
+//
+
+import Foundation
+
+class RockPaperScissorsManager {
+    
+    /// 사용자 입력 유효성 검사 메서드
+    func validateUserInput(_ input: String?) {
+        
+    }
+}
