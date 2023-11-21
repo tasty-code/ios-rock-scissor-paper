41c71a7465ec8543960c358e006ac6db77b35dcb
Changhyun Kang <changhyun.kyle@gmail.com>
2023-11-21 18:12:05 +0900

    refactor: 구조체 타입으로 리팩토링 & 은닉화 및 checkingResult 함수 기능 분리

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,74 +4,76 @@
 //  Copyright © tastycode. All rights reserved.
 //
 
-import Foundation
+struct RockScissorPaper {
+    private let opponentHands: [Int] = [1, 2, 3]
+    private var isRunning: Bool = true
 
-enum Choice: Int {
-    case scissor = 1
-    case rock = 2
-    case paper = 3
-}
+    mutating func choiceRockScissorsPaper() {
+        while isRunning {
+            let computer = checkingOpponentHand()
+            
+            print("가위(1), 바위(2), 보(3)! <종료: 0> :", terminator: " ")
+            guard
+                let input = readLine(),
+                let user = Int(input)
+            else {
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+                continue
+            }
+            checkingResult(user: user, computer: computer)
+            
+        }
+    }
 
-let opponentHands: [Int] = [1, 2, 3]
-var isRunning: Bool = true
-
-func choiceRockScissorsPaper() {
-    
-    while isRunning {
-        let computer = checkingOpponentHand()
-        print(computer)
-        print("가위(1), 바위(2), 보(3)! <종료: 0> :", terminator: " ")
+    private func checkingOpponentHand() -> Int {
         guard
-            let input = readLine(),
-            let user = Int(input)
+            let opponentHand = opponentHands.randomElement()
         else {
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-            continue
+            return 0
         }
         
-        guard
-            let userChoice = Choice(rawValue: user),
-            let computerChoice = Choice(rawValue: computer)
-        else {
-            return
-        }
-        
-        switch user {
-        case 0:
+        return opponentHand
+    }
+
+    private mutating func playGame(user: Choice, computer: Choice) {
+        switch (user, computer) {
+        case (.scissor,.rock),(.rock,.paper),(.paper,.scissor):
+            print("졌습니다!")
             print("게임종료")
-            return
-        case 1...3:
-            playGame(user: userChoice, computer: computerChoice)
+            isRunning = false
+        case (.scissor,.paper),(.rock,.scissor),(.paper,.rock):
+            print("이겼습니다!")
+            print("게임종료")
+            isRunning = false
         default:
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-            continue
+            print("비겼습니다!")
+            choiceRockScissorsPaper()
+        }
+    }
+    
+    private mutating func checkingResult(user: Int, computer: Int) {
+        while isRunning {
+            guard
+                let userChoice = Choice(rawValue: user),
+                let computerChoice = Choice(rawValue: computer)
+            else {
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+                return choiceRockScissorsPaper()
+            }
+            
+            switch user {
+            case 0:
+                print("게임종료")
+                return
+            case 1...3:
+                playGame(user: userChoice, computer: computerChoice)
+            default:
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+                continue
+            }
         }
     }
 }
 
-func checkingOpponentHand() -> Int {
-    guard
-        let opponentHand = opponentHands.randomElement()
-    else {
-        return 0
-    }
-    
-    return opponentHand
-}
-
-func playGame(user: Choice, computer: Choice) {
-    switch (user, computer) {
-    case (.scissor,.rock),(.rock,.paper),(.paper,.scissor):
-        print("졌습니다!")
-        print("게임종료")
-        isRunning = false
-    case (.scissor,.paper),(.rock,.scissor),(.paper,.rock):
-        print("이겼습니다!")
-        print("게임종료")
-        isRunning = false
-    default:
-        print("비겼습니다!")
-    }
-}
-
-choiceRockScissorsPaper()
+var rockScissorPaper = RockScissorPaper()
+rockScissorPaper.choiceRockScissorsPaper()
