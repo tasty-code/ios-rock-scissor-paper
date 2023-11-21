0d647505df7c13c1802ce27ebbda0eaa837dc605
Changhyun Kang <changhyun.kyle@gmail.com>
2023-11-21 10:13:55 +0900

    feat: 승부의 결과를 알 수 있는 playGame() 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,6 +1,6 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by tacocat.
+//  Created by mireu & kyle.
 //  Copyright © tastycode. All rights reserved.
 // 
 
@@ -12,6 +12,8 @@ func choiceRockScissorsPaper() {
     let isRunning: Bool = true
     
     while isRunning {
+        let computer = checkingOpponentHand()
+        print(computer)
         print("가위(1), 바위(2), 보(3)! <종료: 0> :", terminator: " ")
         guard let input = readLine() else { return }
         guard let input2 = Int(input) else { return }
@@ -20,7 +22,7 @@ func choiceRockScissorsPaper() {
         case 0:
             return
         case 1...3:
-            return
+            playGame(user: input2, computer: computer)
         default:
             print("잘못된 입력입니다. 다시 시도해주세요.")
          continue
@@ -34,4 +36,15 @@ func checkingOpponentHand() -> Int {
     return opponentHand
 }
 
+func playGame(user: Int, computer: Int) {
+    switch (user, computer) {
+    case (1,2),(2,3),(3,1):
+        print("졌습니다!")
+    case (1,3),(2,1),(3,2):
+        print("이겼습니다!")
+    default:
+        print("비겼습니다!")
+    }
+}
 
+choiceRockScissorsPaper()
