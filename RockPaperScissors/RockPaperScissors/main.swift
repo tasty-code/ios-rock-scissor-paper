b469bb678938cd6f38d38d9571c758f5daa5f805
kimdj1102 <djkim1102@naver.com>
2023-11-22 16:31:04 +0900

    feat: enum타입, user입력처리 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,19 @@
 
 import Foundation
 
-print("Hello, World!")
 
+enum Hand: String {
+    case scissor = "1" , rock = "2", paper = "3", exit = "0"
+    
+}
+
+func action(com: Hand, user: Hand) {
+    switch user {
+    case .exit:
+        endGame()
+    case .paper, .rock, .scissor:
+        startGame(comPick: com, userPick: user)
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+}
