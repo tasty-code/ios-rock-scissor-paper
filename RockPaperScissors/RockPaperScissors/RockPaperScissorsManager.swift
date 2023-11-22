2104b4029da752bbf44cdeabd2823223f941c8f8
WONGI HONG <dt95@naver.com>
2023-11-22 16:42:49 +0900

    refactor: 변수 타입 변경 및 함수 내 옵셔널 처리 추가

--- RockPaperScissors/RockPaperScissors/RockPaperScissorsManager.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsManager.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 struct RockPaperScissorsManager {
-    private var userValue: Int = 0
-    private var randomValue: Int = 0
-    private var resultValue: Int = 0
+    private var userValue: Int?
+    private var randomValue: Int?
+    private var resultValue: Int?
     private var status: Bool = true
     
     mutating func play() {
@@ -18,15 +18,16 @@ struct RockPaperScissorsManager {
             print("가위(1), 바위(2), 보(3)! <종료: 0> : ")
             
             if let input = readLine() {
-                guard let number = Int(input) else { return }
-                userValue = number
+                userValue = Int(input)
                 
                 switch userValue {
                 case 0:
                     print(Case.exit.rawValue)
                     status = false
                 case 1, 2, 3:
-                    getResult(userValue)
+                    if let userChoice = userValue {
+                        getResult(userChoice)
+                    }
                 default:
                     print(Case.falseValue.rawValue)
                     play()
@@ -37,20 +38,23 @@ struct RockPaperScissorsManager {
     
     mutating func getResult(_ userValue: Int) {
         randomValue = Int.random(in: 1...3)
-        resultValue = userValue - randomValue
         
-        switch resultValue {
-        case 0:
-            print(Case.draw.rawValue)
-            play()
-        case 1, -2:
-            print(Case.win.rawValue)
-            status = false
-        case -1, 2:
-            print(Case.lose.rawValue)
-            status = false
-        default:
-            break
+        if let randomValue {
+            resultValue = userValue - randomValue
+            
+            switch resultValue {
+            case 0:
+                print(Case.draw.rawValue)
+                play()
+            case 1, -2:
+                print(Case.win.rawValue)
+                status = false
+            case -1, 2:
+                print(Case.lose.rawValue)
+                status = false
+            default:
+                break
+            }
         }
     }
 }
