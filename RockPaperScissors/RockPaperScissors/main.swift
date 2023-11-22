94c1f85b272d5a44942803dba4dbfd7da4b8d7c6
Chan Ho Park <park_ch@kakao.com>
2023-11-22 14:29:21 +0900

    feat : 기본 가위바위보 기능 구현.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,29 +6,54 @@
 
 import Foundation
 
-
-
-func randomComputerHand() -> (Int) {
-    return Int.random(in: 1...3)
+enum winDrawLose: String {
+    case win = "이겼습니다!"
+    case draw = "비겼습니다!"
+    case lose = "졌습니다!"
 }
 
-func playerRockPaperScissor() {
-    var userHand = 0
+func randomComputerHand() -> String {
+    return String(Int.random(in: 1...3))
+}
+
+func compareHands(userHand: String?) -> winDrawLose {
+    var result: winDrawLose = winDrawLose.draw
     let comHand = randomComputerHand()
+    if userHand == String(comHand) {
+        result = winDrawLose.draw
+        return result
+    }
+    switch userHand {
+    case "1":
+        result = comHand == "3" ? winDrawLose.win : winDrawLose.lose
+    case "2":
+        result = comHand == "1" ? winDrawLose.win : winDrawLose.lose
+    case "3":
+        result = comHand == "2" ? winDrawLose.win : winDrawLose.lose
+    default :
+        print("잘못된 입력입니다.")
+    }
+    return result
+}
+
+func playRockPaperScissor() {
+    var userInput: String?
     repeat {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-        if let input = readLine(), let playerInput = Int(input) {
-            print("플레이어 선택 \(playerInput)")
-            print("컴퓨터의 선택 \(comHand)")
-        }
-        
-        
-        if userHand == 0 {
+        var result: String
+        userInput = readLine()
+        switch userInput {
+        case "0":
             print("게임 종료")
-            break
+        case "1", "2", "3":
+    
+                result = compareHands(userHand: userInput).rawValue
+                print(result)
+            default : print("잘못된 입력입니다. 다시 시도해주세요.")
         }
-    } while userHand != 0
+        
+    } while userInput != "0"
 }
 
 
-playerRockPaperScissor()
+playRockPaperScissor()
