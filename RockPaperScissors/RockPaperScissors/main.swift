840cd71fd42c7823cc82b55b6c00b1b46a4af8bd
Paul Lee <earths-voluble.0g@icloud.com>
2023-11-22 14:58:45 +0900

    :recycle: 에러 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -16,29 +16,33 @@ enum Selection: Int {
 
 while true {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :")
-    if let input = readLine() {
-        
+    if let input = readLine(), let select = Selection(rawValue: input) {
+        handleSelection(select)
+    } else {
+        print("1~3 을 입력해주세요")
     }
 }
 
-func handleSelection(_ select: Selection) {
-    switch select {
-    case.exit:
-        
+func handleSelection(_ enumValue: Selection) {
+    switch enumValue {
+    case .exit:
+        runningGame(0)
     case .scissors:
-        
+        runningGame(1)
     case .rock:
-        
+        runningGame(2)
     case .paper:
-        
+        runningGame(3)
     }
 }
-func runningGame(_ myChoice: Selection) {
+func runningGame(_ myChoice: Int) {
     var comChoice = Int.random(in: 1...3)
     if comChoice == myChoice {
         print("비겼습니다!")
-    } else if {
-        comChoice = 1 && myChoice = 2
+    } else if comChoice = 1 && myChoice = 2 {
+        print("이겼습니다!")
+    } else {
+        print("졌습니다")
     }
     
 }
