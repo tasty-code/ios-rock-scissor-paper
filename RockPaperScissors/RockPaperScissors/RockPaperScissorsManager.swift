da76200ffc79fd84c72043fdf9c84da89197d74f
eunjuchoi <eunjuzzoo@naver.com>
2023-11-22 14:15:08 +0900

    feat: 사용자 입력 유효성 검사 메서드 구현 (#1)

--- RockPaperScissors/RockPaperScissors/RockPaperScissorsManager.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsManager.swift
@@ -11,6 +11,16 @@ class RockPaperScissorsManager {
     
     /// 사용자 입력 유효성 검사 메서드
     func validateUserInput(_ input: String?) -> Int {
-        return -1
+        guard let input else {
+            return -1
+        }
+        
+        switch input {
+        case "0": return 0
+        case "1": return 1
+        case "2": return 2
+        case "3": return 3
+        default: return -1
+        }
     }
 }
