af20d777ea31178053d23b3ec5fcb81012ddf2c5
eunjuchoi <eunjuzzoo@naver.com>
2023-11-22 14:17:10 +0900

    chore: main 파일 코드 수정 (#1)
    - RockPaperScissorsManager 객체 생성
    - 사용자 입력 판단 코드 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,13 +7,15 @@
 import Foundation
 
 let ioManager = IOManager()
+let rockPaperScissorsManager = RockPaperScissorsManager()
 var computerChoice = Int.random(in: 1...3)
 
 func playGame() {
     while true {
         ioManager.showMessage(.default)
-        let input = readLine()
         
+        let input = readLine()
+        let result = rockPaperScissorsManager.validateUserInput(input)
         
     }
 }
