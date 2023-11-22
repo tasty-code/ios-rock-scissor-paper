1aca63d513e8471dec11ab569b6a1834cc0bca52
ujhong7 <ujhong7>
2023-11-22 13:54:59 +0900

    :building_construction: 선택받을 열거형 및 커맨드라인 출력문구 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,7 +6,21 @@
 
 import Foundation
 
-print("Hello, World!")
+enum Selection: String {
+    case exit = "0"
+    case scissors = "1"
+    case rock = "2"
+    case paper = "3"
+    
+}
 
-print("test")
-print("test2")
+while true {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> :")
+    if let input = readLine() {
+        
+    }
+}
+
+func handleSelection() {
+    
+}
