bc14089e5b35fbbec435abe2ef7c2b4638b0d7b3
WONGI HONG <dt95@naver.com>
2023-11-22 16:53:14 +0900

    chore: 출력문 내용 변경 및 불필요한 코드 삭제

--- RockPaperScissors/RockPaperScissors/RockPaperScissorsManager.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsManager.swift
@@ -15,7 +15,7 @@ struct RockPaperScissorsManager {
     
     mutating func play() {
         while status {
-            print("가위(1), 바위(2), 보(3)! <종료: 0> : ")
+            print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
             
             if let input = readLine() {
                 userValue = Int(input)
@@ -30,7 +30,6 @@ struct RockPaperScissorsManager {
                     }
                 default:
                     print(Case.falseValue.rawValue)
-                    play()
                 }
             }
         }
@@ -39,13 +38,12 @@ struct RockPaperScissorsManager {
     mutating func getResult(_ userValue: Int) {
         randomValue = Int.random(in: 1...3)
         
-        if let randomValue {
+        if let randomValue = randomValue {
             resultValue = userValue - randomValue
             
             switch resultValue {
             case 0:
                 print(Case.draw.rawValue)
-                play()
             case 1, -2:
                 print(Case.win.rawValue)
                 status = false
