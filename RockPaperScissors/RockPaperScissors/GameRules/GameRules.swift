15e4ebbe70a63045833a70b2c918430a0f616bd7
suojae3 <suojae3@naver.com>
2023-11-22 21:28:07 +0900

    [FIX]: determineWinner 리턴값 에러 수정

--- RockPaperScissors/RockPaperScissors/GameRules/GameRules.swift
+++ RockPaperScissors/RockPaperScissors/GameRules/GameRules.swift
@@ -48,8 +48,8 @@ extension GameRules {
         guard let computerNode = rpsLinkedList.node(for: computerChoice) 
         else { return "컴퓨터 에러!"}
         
-        if computerNode.next?.value == userChoice   { return "승 리" }
+        if computerNode.next?.value == userChoice   { return "승리" }
         else if computerNode.value == userChoice    { return "무승부" }
-        else                                        { return "패 배" }
+        else                                        { return "패배" }
     }
 }
