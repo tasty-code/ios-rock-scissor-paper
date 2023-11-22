d1d0d6c4cd7a7e52b8fbc11cc225cc8dc50048a3
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-11-22 13:53:56 +0900

    feat: 믁찌빠 변수 생성

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsManager.swift
@@ -0,0 +1,14 @@
+//
+//  RockPaperScissorsManager.swift
+//  RockPaperScissors
+//
+//  Created by nayeon  on 2023/11/22.
+//
+
+import Foundation
+
+struct RockPaperScissorsManager {
+    private var userValue: Int
+    private var randomValue: Int
+    private var status: Bool = true
+}
