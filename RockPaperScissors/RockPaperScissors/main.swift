5fa7ed9bbdf3ab9eaed85f5f41a82a20c580dfdb
AsherKim <tawans@naver.com>
2023-11-22 10:15:08 +0900

    feat: choice 배열 선언 및 while 문에서 사용자에게 입력 받아 출력하기 작성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -2,11 +2,29 @@
 //  RockPaperScissors - main.swift
 //  Created by tacocat.
 //  Copyright © tastycode. All rights reserved.
-// 
+//
 
 import Foundation
 
-print("Hello, World!")
+let choice = ["rock", "scissors", "paper"]
 
-print("레포지토리 연결테스트")
+var isEnd = false
 
+while(isEnd == false) {
+    print("바위(1) 가위(2) 보(3) 중 선택하시오!")
+    let playderChoice = Int(readLine()!)
+    
+    switch playderChoice {
+    case 1: print(choice[0])
+        break
+    case 2: print(choice[1])
+        break
+    case 3: print(choice[2])
+        break
+
+    default: print("잘못된 입력값입니다.")
+    
+    }
+
+    isEnd = true
+}
