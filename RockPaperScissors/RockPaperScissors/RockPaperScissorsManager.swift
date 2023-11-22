dbb20cc8246231a5b8b572d32c85e105b53604c7
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-11-22 14:18:05 +0900

    feat: 출력문 작성

--- RockPaperScissors/RockPaperScissors/RockPaperScissorsManager.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsManager.swift
@@ -11,4 +11,15 @@ struct RockPaperScissorsManager {
     private var userValue: Int
     private var randomValue: Int
     private var status: Bool = true
+    
+    mutating func play() {
+        while status {
+            print("가위(1), 바위(2), 보(3)! <종료: 0> : ")
+            
+            if let input = readLine() {
+                guard let number = Int(input) else { return }
+                userValue = number
+            }
+        }
+    }
 }
