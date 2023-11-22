cb0e205135f8ad5be6f9cf3e2c31f547ccc5111c
LeeSe0ngYe0n <goblin__@naver.com>
2023-11-22 19:05:31 +0900

    feat: switch문으로 유저와 컴퓨터의 게임결과

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -26,3 +26,17 @@ func rpsMenu() {
 func comRandomInput() -> Int{
     return Int.random(in: 1 ... 3)
 }
+
+func compareRps(user: RpsCase, com: RpsCase) {
+    switch (user, com) {
+    case (.scissors, .paper), (.rock, .scissors), (.paper, .rock):
+        print("이겼습니다.")
+        print("게임 종료")
+    case (.paper, .scissors), (.scissors, .rock), (.rock, .paper):
+        print("졌습니다.")
+        print("게임 종료")
+    default:
+        print("비겼습니다.")
+        rpsMenu()
+    }
+}
