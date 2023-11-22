15ac32025a163e743bc7e68bd1b25345a0b820f1
ujhong7 <ujhong7>
2023-11-22 15:12:54 +0900

    :triangular_flag_on_post: Step 1 완료

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,12 +6,11 @@
 
 import Foundation
 
-enum Selection: Int {
-    case exit = 0
-    case scissors = 1
-    case rock = 2
-    case paper = 3
-    
+enum Selection: String {
+    case exit = "0"
+    case scissors = "1"
+    case rock = "2"
+    case paper = "3"
 }
 
 while true {
@@ -19,14 +18,14 @@ while true {
     if let input = readLine(), let select = Selection(rawValue: input) {
         handleSelection(select)
     } else {
-        print("1~3 을 입력해주세요")
+        print("잘못된 입력입니다. 다시 시도해주세요.")
     }
 }
 
 func handleSelection(_ enumValue: Selection) {
     switch enumValue {
     case .exit:
-        runningGame(0)
+        exit(0)
     case .scissors:
         runningGame(1)
     case .rock:
@@ -35,14 +34,22 @@ func handleSelection(_ enumValue: Selection) {
         runningGame(3)
     }
 }
+
 func runningGame(_ myChoice: Int) {
     var comChoice = Int.random(in: 1...3)
     if comChoice == myChoice {
         print("비겼습니다!")
-    } else if comChoice = 1 && myChoice = 2 {
+    } else if comChoice == 1 && myChoice == 2 {
         print("이겼습니다!")
+        exit(0)
+    } else if comChoice == 2 && myChoice == 3 {
+        print("이겼습니다!")
+        exit(0)
+    } else if comChoice == 3 && myChoice == 1 {
+        print("이겼습니다!")
+        exit(0)
     } else {
-        print("졌습니다")
+        print("졌습니다!")
+        exit(0)
     }
-    
 }
