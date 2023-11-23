db98734438cfc69f19a3d390e4038eb97f1ca212
ijs1103 <ajaijs1103@naver.com>
2023-11-23 14:29:52 +0900

    feat(*): Result 파일 생성

---
+++ RockPaperScissors/RockPaperScissors/Models/Result.swift
@@ -0,0 +1,15 @@
+//
+//  Result.swift
+//  RockPaperScissors
+//
+//  Created by 이주상 on 2023/11/23.
+//
+
+import Foundation
+
+enum Result: String {
+    case win = "이겼습니다!"
+    case lose = "졌습니다!"
+    case draw = "비겼습니다!"
+    case none = "에러상황"
+}
