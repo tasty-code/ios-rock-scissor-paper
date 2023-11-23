e59f4e429107e406e51200f9846b13cd341c5f60
Andy An <andyanappdev@gmail.com>
2023-11-23 14:39:10 +0900

    feat(*): 콘솔 출력을 위한 Constant 정의

---
+++ RockPaperScissors/RockPaperScissors/Constant.swift
@@ -0,0 +1,16 @@
+//
+//  Constant.swift
+//  RockPaperScissors
+//
+//  Created by Doyoung An on 11/23/23.
+//
+
+import Foundation
+
+struct Constant {
+    static let rpsChoice: String = "가위(1), 바위(2), 보(3)! <종료 : 0> :"
+    
+    static let gameExit: String = "게임 종료"
+    
+    static let badInput: String = "잘못된 입력입니다. 다시 시도해주세요"
+}
