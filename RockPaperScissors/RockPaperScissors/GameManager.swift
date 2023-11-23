f2027a837b2a80ba57b87e5de32d3aee37002fd9
hemil0102 <hemil0102@gmail.com>
2023-11-23 14:57:29 +0900

    modeling: GameManager에서GameOutcome 열거형 타입 구현

--- RockPaperScissors/RockPaperScissors/GameManager.swift
+++ RockPaperScissors/RockPaperScissors/GameManager.swift
@@ -12,6 +12,21 @@ struct GameManager {
         case paper = 3
     }
     
+    private enum GameOutcome {
+        case draw, win, loss
+        
+        func notify() {
+            switch self {
+            case .draw:
+                print("비겼습니다!")
+            case .win:
+                print("이겼습니다!")
+            case .loss:
+                print("졌습니다!")
+            }
+        }
+    }
+    
     let termination: String = "0"
     var willRun: Bool = true
     
