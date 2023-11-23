d6d7076c9b57aad40b3a99a2b376c2b988cf1870
llamang <yohanlee1230@daum.net>
2023-11-23 14:45:21 +0900

    modeling: GameManager 구조체에 RockPaperScissors 열거형 타입 및 일부 프로퍼티 구현

--- RockPaperScissors/RockPaperScissors/GameManager.swift
+++ RockPaperScissors/RockPaperScissors/GameManager.swift
@@ -6,6 +6,15 @@
 import Foundation
 
 struct GameManager {
+    private enum RockPaperScissors: Int, CaseIterable {
+        case scissors = 1
+        case rock = 2
+        case paper = 3
+    }
+    
+    let termination: String = "0"
+    var willRun: Bool = true
+    
     func canPlayGame() -> Bool {
         return true
     }
