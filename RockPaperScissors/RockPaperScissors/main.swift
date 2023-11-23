d4b484c759e014a097dce0a55db28659cd5174fd
LeeSe0ngYe0n <goblin__@naver.com>
2023-11-23 16:40:26 +0900

    refactor: 변수명 수정 및 프린트 출력 문구 네임스페이스로 관리

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,62 +6,64 @@
 
 import Foundation
 
-enum RpsCase: Int {
-    case scissors = 1
-    case rock
-    case paper
+enum RockScissorsPaperCase: Int {
+    case scissors = 1, rock, paper
+}
+
+enum StringNameSpace: String {
+    case winMessage = "이겼습니다."
+    case loseMessage = "졌습니다."
+    case drawMessage = "비겼습니다."
+    case endMessage = "게임 종료"
+    case inputErrorMessage = "잘못된 입력입니다. 다시 시도해주세요."
+    case rockScissorsPaperExit = "가위(1), 바위(2) 보(3)! <종료: 0>"
 }
 
 var isPlayRockScissorsPaperGame = true
 
-
-userInput()
-
+initPlayRockScissorsPaperGame()
 
 // MARK: - Function
 func displayRockScissorsPaperMenu() {
-    print("가위(1), 바위(2) 보(3)! <종료: 0>")
+    print(StringNameSpace.rockScissorsPaperExit.rawValue)
 }
 
-func comRandomInput() -> Int {
+private func comRandomInput() -> Int {
     return Int.random(in: 1 ... 3)
 }
 
-func compareRockScissorsPaper(user: RpsCase, com: RpsCase) {
+private func compareRockScissorsPaper(user: RockScissorsPaperCase, com: RockScissorsPaperCase) {
     switch (user, com) {
     case (.scissors, .paper), (.rock, .scissors), (.paper, .rock):
-        print("이겼습니다.")
-        print("게임 종료")
+        print(StringNameSpace.winMessage.rawValue)
+        print(StringNameSpace.endMessage.rawValue)
         isPlayRockScissorsPaperGame = false
     case (.paper, .scissors), (.scissors, .rock), (.rock, .paper):
-        print("졌습니다.")
-        print("게임 종료")
+        print(StringNameSpace.loseMessage.rawValue)
+        print(StringNameSpace.endMessage.rawValue)
         isPlayRockScissorsPaperGame = false
     default:
-        print("비겼습니다.")
+        print(StringNameSpace.drawMessage.rawValue)
         displayRockScissorsPaperMenu()
     }
 }
 
-
-func userInput() {
+private func userInput() {
     while isPlayRockScissorsPaperGame {
         let randomValue = comRandomInput()
         guard let inputValue = readLine() else { return }
         if let num = Int(inputValue), num >= 1 && num <= 3 {
-            compareRockScissorsPaper(user: RpsCase(rawValue:num)!, com: RpsCase(rawValue:randomValue)!)
+            compareRockScissorsPaper(user: RockScissorsPaperCase(rawValue:num)!, com: RockScissorsPaperCase(rawValue:randomValue)!)
         } else if let num = Int(inputValue) , num == 0 {
-            print("게임 종료")
+            print(StringNameSpace.endMessage)
             isPlayRockScissorsPaperGame = false
         } else {
-            print("잘못된 입력입니다. 다시 시도해주세요.")
+            print(StringNameSpace.inputErrorMessage.rawValue)
         }
     }
 }
 
 func initPlayRockScissorsPaperGame() {
     displayRockScissorsPaperMenu()
-    while isPlayRockScissorsPaperGame {
-        userInput()
-    }
+    userInput()
 }
