e23ccb078924b4ee92b4c25b4373b4b73b6cc2b2
Andy An <andyanappdev@gmail.com>
2023-11-23 15:16:44 +0900

    feat:(Game): Play Rps 메서드 구조 정의
    - 게임 진행의 핵심 메서드의 구조를 정의
    - shutDown 메서드 구현
    - breakLoop 메서드 구현

--- RockPaperScissors/RockPaperScissors/Game.swift
+++ RockPaperScissors/RockPaperScissors/Game.swift
@@ -9,14 +9,28 @@ import Foundation
 
 final class Game {
     private var loopState: Bool = true
+    
     private var result: Result = .none
+    
     private var inputNum: Int = 0
     
     private func playRps() {
         while loopState {
+            
             print(Constant.rpsChoice)
             
             setInputNum()
+            
+            switch inputNum {
+            case 1, 2, 3:
+                // TODO: 승패 확인 로직 메서드 콜
+                print(result.rawValue)
+            case 0:
+                // TODO: 겜임 exit 메서드 콜
+                print(Constant.gameExit)
+            default:
+                print(Constant.badInput)
+            }
         }
     }
     
@@ -26,5 +40,14 @@ final class Game {
             return
         }
         self.inputNum = inputNum
+    }
+    
+    private func shutDown() {
+        print(Constant.gameExit)
+        breakLoop()
+    }
+    
+    private func breakLoop() {
+        self.loopState = false
     }
 }
