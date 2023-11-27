4dfc14161f17cffac101b96bc5ba0436efa64d63
Hoi.Kwon <teaho177@gmail.com>
2023-11-27 22:04:53 +0900

    feat: 코드 피드백 사항 반영 및 메소드명 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,63 +1,55 @@
-
 import Foundation
 
-enum ChoiceMenu: String {
-    case scissor = "1" , rock = "2", paper = "3", exit = "0"
-    case other
+enum ChoiceMenu: String, CaseIterable {
+    case scissor = "1", rock = "2", paper = "3", exit = "0"
 }
 
 enum Result: String {
-    case win = "이겼습니다!"
-    case lose = "졌습니다!"
-    case draw = "비겼습니다!"
-}
-
-func startMessage() {
-    print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
-}
-
-func action(com: ChoiceMenu, user: ChoiceMenu) {
-    switch user {
-    case .exit:
-        endGame()
-    case .paper, .rock, .scissor:
-        startGame(comPick: com, userPick: user)
-    case .other:
-        print("잘못된 입력입니다. 다ls시 시도해주세요.")
-    }
-}
-
-func startGame(comPick: ChoiceMenu, userPick: ChoiceMenu) {
-    if comPick == userPick {
-        print(Result.draw.rawValue)
-        return
-    } else if (comPick == .scissor && userPick == .rock) ||
-                (comPick == .rock && userPick == .paper) ||
-                (comPick == .paper && userPick == .scissor)
-    {
-        print(Result.win.rawValue)
-    } else {
-        print(Result.lose.rawValue)
-    }
-    
-    endGame()
+    case win = "이겼습니다."
+    case lose = "졌습니다."
+    case draw = "비겼습니다."
+    case errorDefault = "잘못된 입력입니다. 다시 시도해주세요."
+    case quitGame = "게임종료"
 }
 
 func endGame() {
     print("게임 종료")
-    isWorking.toggle()
+    isWorking = false
 }
 
-var isWorking: Bool = true
-let rps: [String] = ["1", "2", "3"]
+func displayStartMessage() {
+    print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
+}
 
-while isWorking {
-    startMessage()
-    let comChoice: String = rps.randomElement() ?? ""
-    
-    if let userChoice = readLine() {
-        action(com: ChoiceMenu(rawValue: comChoice) ?? .scissor, user: ChoiceMenu(rawValue: userChoice) ?? .other)
+//함수명 변경 startGmae -> calculateResult
+func calculateResult(comInput: ChoiceMenu, userInput: ChoiceMenu) -> Result {
+    if userInput == comInput {
+        return .draw
+    } else if (comInput == .scissor && userInput == .rock) ||
+                (comInput == .rock && userInput == .paper) ||
+                (comInput == .paper && userInput == .scissor) {
+        return .win
+    } else {
+        return .lose
     }
 }
 
+var isWorking: Bool = true
 
+while isWorking {
+    displayStartMessage()
+    
+    if let userInput = readLine(), let userPick = ChoiceMenu(rawValue: userInput) {
+        let comPick = ChoiceMenu.allCases.randomElement() ?? .scissor
+        
+        //결과값 받는 메서드 추가
+        let result: Result = calculateResult(comInput: comPick, userInput: userPick)
+        print(result.rawValue)
+        
+        if result == .quitGame || result == .win || result == .lose {
+            endGame()
+        }
+    } else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+}
