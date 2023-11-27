db62f3bf1d981c6768b95eb19e0356a28824fc75
mireu79 <https://github.com/mireu79>
2023-11-27 16:38:35 +0900

    feat: CaseIterable 프로토콜 채택

--- RockPaperScissors/RockPaperScissors/NameSpace/GameChoice.swift
+++ RockPaperScissors/RockPaperScissors/NameSpace/GameChoice.swift
@@ -5,13 +5,13 @@
 //  Created by mireu & kyle.
 //
 
-enum RockScissorPaperChoice: Int {
+enum RockScissorPaperChoice: Int, CaseIterable {
     case scissor = 1
     case rock = 2
     case paper = 3
 }
 
-enum MukJjiPpaChoice: Int {
+enum MukJjiPpaChoice: Int, CaseIterable {
     case muk = 1
     case jji = 2
     case ppa = 3
