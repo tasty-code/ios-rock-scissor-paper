a94f73300408e89473865a6cc17a2ca0b383a01a
eunjuchoi <eunjuzzoo@naver.com>
2023-11-27 11:22:49 +0900

    chore: 필요없는 코드 삭제 (#2)

--- RockPaperScissors/RockPaperScissors/Enum/UserGuideMessage.swift
+++ RockPaperScissors/RockPaperScissors/Enum/UserGuideMessage.swift
@@ -13,20 +13,4 @@ enum UserGuideMessage {
     case lose
     case draw
     case exit
-    
-    func showMessage() {
-        switch self {
-            
-        case .default:
-            print("가위(1), 바위(2), 보(3)! <종료 : 0> : ")
-        case .win:
-            print("이겼습니다!")
-        case .lose:
-            print("졌습니다!")
-        case .draw:
-            print("비겼습니다!")
-        case .exit:
-            print("게임 종료")
-        }
-    }
 }
