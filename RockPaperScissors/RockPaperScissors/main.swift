acbde2000ca6122e8952c8bce98e9bd24a14bc09
eunjuchoi <eunjuzzoo@naver.com>
2023-11-27 18:59:04 +0900

    chore: main.swift 파일 코드 수정 (#2)

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,31 +7,14 @@
 import Foundation
 
 let rockPaperScissorsManager = RockPaperScissorsManager()
-var computerChoice = Int.random(in: 1...3)
+let advancedRockPaperScissorsManager = AdvancedRockPaperScissorsManager(rockPaperScissorsManager: rockPaperScissorsManager)
 
-func playGame() {
-    while true {
-        rockPaperScissorsManager.showMessage(.default)
-        
-        let input = readLine()
-        let userChoice: RockPaperScissorsType
-        
-        do {
-            userChoice = try rockPaperScissorsManager.validateUserInput(input)
-        } catch {
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-            continue
-        }
-        
-        let gameResult = rockPaperScissorsManager.judgeGame(user: userChoice,
-                                                            computer: RockPaperScissorsType(rawValue: computerChoice))
-        rockPaperScissorsManager.showMessage(gameResult)
-        
-        if gameResult != .draw {
-            return
-        }
-    }
+let rockPaperScissorsResult = rockPaperScissorsManager.playGame(.default)
+
+switch rockPaperScissorsResult {
+case .exit:
+    break
+default:
+    advancedRockPaperScissorsManager.playGame(rockPaperScissorsResult)
 }
-
-playGame()
 
