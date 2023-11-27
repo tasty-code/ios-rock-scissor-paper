1c6f9a75fec4070c2eb258a72dcb8d2f5abc57bf
eunjuchoi <eunjuzzoo@naver.com>
2023-11-27 18:44:47 +0900

    feat: 가위바위보 타입을 묵찌빠 타입으로 변경하는 메서드 추가 (#2)

--- RockPaperScissors/RockPaperScissors/Enum/RockPaperScissorsType.swift
+++ RockPaperScissors/RockPaperScissors/Enum/RockPaperScissorsType.swift
@@ -13,4 +13,17 @@ enum RockPaperScissorsType: Int {
     case scissors = 1
     case rock = 2
     case paper = 3
+    
+    func getAdvancedType() -> RockPaperScissorsType {
+        switch self {
+        case .none:
+            return .none
+        case .scissors:
+            return .rock
+        case .rock:
+            return .scissors
+        case .paper:
+            return .paper
+        }
+    }
 }
