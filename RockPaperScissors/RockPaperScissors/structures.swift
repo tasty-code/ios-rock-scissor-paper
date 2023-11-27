81514aea38fc05707be7e89bbb3b59bee49617cd
Chan Ho Park <park_ch@kakao.com>
2023-11-27 11:28:12 +0900

    feat : Player 구조체 생성
    1. name = 플레이어 이름을 저장
    2. rockScissorPaper = 가위바위보 결과값을 저장
    3. mookJjiBba = 묵찌빠 결과값을 저장
    
    위 프로퍼티 들의 get, set 정의

--- RockPaperScissors/RockPaperScissors/structures.swift
+++ RockPaperScissors/RockPaperScissors/structures.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+struct Player {
+    private var name: String
+    private var rockScissorPaper: RockScissorPaper
+    private var mookJjiBba : MookJjiBba
+    
+    init(name: String, rockScissorPaper: RockScissorPaper, mookJjiBba: MookJjiBba) {
+        self.name = name
+        self.rockScissorPaper = rockScissorPaper
+        self.mookJjiBba = mookJjiBba
+    }
+    
+    init(name: String) {
+        self.init(name: name, rockScissorPaper: .paper, mookJjiBba: .paper)
+    }
+    
+    func getName() -> String {
+        return self.name
+    }
+    
+    func getRockScissorPaper() -> RockScissorPaper {
+        return self.rockScissorPaper
+    }
+    
+    func getMookJjiBba() -> MookJjiBba {
+        return self.mookJjiBba
+    }
+    
+    mutating func setRockScissorPaper(input: Int) {
+        self.rockScissorPaper = RockScissorPaper(rawValue: input) ?? .paper
+    }
+    
+    mutating func setMookJjiBba(input: Int) {
+        self.mookJjiBba = MookJjiBba(rawValue: input) ?? .paper
+    }
+}
