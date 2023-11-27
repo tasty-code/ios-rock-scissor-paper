958e5e580de1bbc8d8a51b1f4e81fcacf3eaaa91
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-11-27 17:57:01 +0900

    refactor: 네이밍 변경 및 메소드 간결하게 코드 수정

--- RockPaperScissors/RockPaperScissors/RockPaperScissorsManager.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsManager.swift
@@ -17,25 +17,24 @@ struct RockPaperScissorsManager {
         while status {
             print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
             
-            if let input = readLine() {
-                userValue = Int(input)
-                
-                switch userValue {
-                case 0:
-                    print(Case.exit.rawValue)
-                    status = false
-                case 1, 2, 3:
-                    if let userChoice = userValue {
-                        getResult(userChoice)
-                    }
-                default:
-                    print(Case.falseValue.rawValue)
+            guard let input = readLine() else { return }
+            userValue = Int(input)
+            
+            switch userValue {
+            case 0:
+                print(GameResult.exit.rawValue)
+                status = false
+            case 1, 2, 3:
+                if let userChoice = userValue {
+                    getResult(userValue: userChoice)
                 }
+            default:
+                print(GameResult.wrongValue.rawValue)
             }
         }
     }
-    
-    mutating func getResult(_ userValue: Int) {
+
+    mutating func getResult(userValue: Int) {
         randomValue = Int.random(in: 1...3)
         
         if let randomValue = randomValue {
@@ -43,12 +42,12 @@ struct RockPaperScissorsManager {
             
             switch resultValue {
             case 0:
-                print(Case.draw.rawValue)
+                print(GameResult.draw.rawValue)
             case 1, -2:
-                print(Case.win.rawValue)
+                print(GameResult.win.rawValue)
                 status = false
             case -1, 2:
-                print(Case.lose.rawValue)
+                print(GameResult.lose.rawValue)
                 status = false
             default:
                 break
