645deae832173db50557c7fe03db63a5f662fe14
eensungkim <eensungkim@gmail.com>
2023-11-28 11:31:37 +0900

    refactor: InGameMessage 싱글턴 추가에 따른 enumerations 수정

--- RockPaperScissors/RockPaperScissors/enumerations.swift
+++ RockPaperScissors/RockPaperScissors/enumerations.swift
@@ -8,9 +8,7 @@
 import Foundation
 
 enum GameResult: String {
-    case win = "이겼습니다!"
-    case draw = "비겼습니다!"
-    case lose = "졌습니다!"
+    case win, draw, lose
 }
 
 enum RockScissorPaper: Int {
@@ -22,50 +20,9 @@ enum MookJjiBba: Int {
 }
 
 enum GameType: String {
-    case rockScissorPaper = "가위(1), 바위(2), 보(3)! <종료 : 0> : "
-    case mookJjiBba = "묵(1), 찌(2), 빠(3)! <종료 : 0> : "
-    
-    func message(gameType: GameType, turn player: Player) -> String {
-        switch gameType {
-        case .rockScissorPaper:
-            return gameType.rawValue
-        case .mookJjiBba:
-            return "[\(player.getName()) 턴] " + gameType.rawValue
-        }
-    }
+    case rockScissorPaper, mookJjiBba
 }
 
-enum InGameMessage {
-    case ready, evaluation, falseInput
-    
-    func message() {
-        switch self {
-        case .falseInput:
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-        default:
-            print("상태가 정의되지 않았습니다.")
-        }
-    }
-    
-    func message(for gameType: GameType, by gameResult: GameResult) {
-        switch (gameType, self) {
-        case (.rockScissorPaper, .ready):
-            print("가위(1), 바위(2), 보(3)! <종료 : 0> : ")
-        case (.rockScissorPaper, .evaluation):
-            print(gameResult.rawValue)
-        default:
-            print("상태가 정의되지 않았습니다.")
-        }
-    }
-    
-    func message(for gameType: GameType, turn player: Player) {
-        switch (gameType, self) {
-        case (.mookJjiBba, .ready):
-            print("[\(player.getName()) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ")
-        case (.mookJjiBba, .evaluation):
-            print("\(player.getName())의 턴입니다.")
-        default:
-            print("상태가 정의되지 않았습니다.")
-        }
-    }
+enum GameStatus {
+    case ready, evaluation, falseInput, completed
 }
