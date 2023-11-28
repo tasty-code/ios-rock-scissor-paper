96790c2428d2139eee0deccaa2e0793d98836e37
Hoi.Kwon <teaho177@gmail.com>
2023-11-29 01:25:04 +0900

    지움

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -29,19 +29,18 @@ func calculateResult(comInput: ChoiceMenu, userInput: ChoiceMenu) -> Result {
 var isWorking: Bool = true
 
 while isWorking {
+    
     displayStartMessage()
     
     if let userInput = readLine(), let userPick = ChoiceMenu(rawValue: userInput) {
         guard let comPick = ChoiceMenu.allCases.filter({ $0 != .exit }).randomElement() else { continue }
-        
         //결과값 받는 메서드 추가
         let result: Result = calculateResult(comInput: comPick, userInput: userPick)
         print(result.rawValue)
         
         
         if result == .win || result == .lose {
-            playMukccippa()
-            
+            endGame()
         } else if result == .quitGame {
             isWorking = false
         } else if result == .draw {
@@ -56,53 +55,5 @@ while isWorking {
 
 // MARK: - 묵찌빠
 
-//var player:Bool = true
-////var turn: String = player == true ? "사용자" : "컴퓨터"
 
 
-//func displaymukjjippaGameMessage(forTurn: String) {
-//    print("[\(forTurn) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :")
-//}
-//
-//let isplayerTurn = true
-//
-//func vicmessage(player: String) {
-//    print("\(player)의 승리")
-//}
-//
-//var mukccippaGame: Bool = false
-
-
-//func playMukccippa() {
-//    
-//    let turn = isplayerTurn ? "사용자턴" : "플레이어턴"
-//    
-//    displaymukjjippaGameMessage(forTurn: turn)
-//    
-//    while true {
-//        
-//        if let userInput = readLine(), let userPick = ChoiceMenu(rawValue: userInput) {
-//            guard let comPick = ChoiceMenu.allCases.filter({ $0 != .exit }).randomElement() else { continue }
-//            let result: Result = calculateResult(comInput: comPick, userInput: userPick)
-//            print(result.rawValue)
-//            
-//            if result == .draw && isplayerTurn == true {
-//                print("[사용자]가 이겼습니다.")
-//            }
-//        }
-//    }
-//}
-
-
-class MukJjiPpaGame {
-    var isWorking = true
-    var isPlayerTurn = true
-    
-    func displayMukjjippaGameMessage(turn: String) {
-        print("[\(turn) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :")
-    }
-    
-    func resultCalculate(userInput: Int, comInput: Int) -> Result {
-        
-    }
-}
