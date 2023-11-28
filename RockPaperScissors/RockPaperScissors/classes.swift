28a8a14062b578f2a5759b59d66e37a6f132012e
eensungkim <eensungkim@gmail.com>
2023-11-28 11:47:59 +0900

    fix: 묵찌빠 승리시 InGameMessage 추가

--- RockPaperScissors/RockPaperScissors/classes.swift
+++ RockPaperScissors/RockPaperScissors/classes.swift
@@ -16,7 +16,8 @@ class InGameMessage {
         "evaluation,Optional(RockPaperScissors.GameType.rockScissorPaper),Optional(RockPaperScissors.GameResult.win)" : { _ in "이겼습니다!" },
         "evaluation,Optional(RockPaperScissors.GameType.rockScissorPaper),Optional(RockPaperScissors.GameResult.draw)" : { _ in "비겼습니다!" },
         "evaluation,Optional(RockPaperScissors.GameType.rockScissorPaper),Optional(RockPaperScissors.GameResult.lose)" : { _ in "졌습니다!" },
-        "evaluation,Optional(RockPaperScissors.GameType.mookJjiBba),nil" : { playerName in "\(playerName)의 턴입니다." },
+        "evaluation,Optional(RockPaperScissors.GameType.mookJjiBba),Optional(RockPaperScissors.GameResult.draw" : { playerName in "\(playerName)의 턴입니다." },
+        "evaluation,Optional(RockPaperScissors.GameType.mookJjiBba),Optional(RockPaperScissors.GameResult.win" : { playerName in "\(playerName)의 승리!" },
         "falseInput,nil,nil" : { _ in "잘못된 입력입니다. 다시 시도해주세요." },
         "completed,nil,nil" : { _ in "게임 종료" }
     ]
