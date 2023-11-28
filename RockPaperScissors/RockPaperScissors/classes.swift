f6098d3701510f139f75fbd58fd267fd028d7a06
eensungkim <eensungkim@gmail.com>
2023-11-28 11:50:17 +0900

    refactor : GameMaster 구조체 클래스로 변환
    싱글톤 개념 적용하기 위한 준비작업

--- RockPaperScissors/RockPaperScissors/classes.swift
+++ RockPaperScissors/RockPaperScissors/classes.swift
@@ -35,3 +35,92 @@ class InGameMessage {
         return self.inGameMessage
     }
 }
+
+class GameMaster {
+    private var gameType: GameType
+    private var user: Player
+    private var computer: Player
+    private var turn: Player
+    
+    init(gameType: GameType, user: Player, computer: Player, turn: Player) {
+        self.gameType = gameType
+        self.user = user
+        self.computer = computer
+        self.turn = turn
+    }
+    
+    func getGameType() -> GameType {
+        return self.gameType
+    }
+    
+    func setGameType(to gameType: GameType) {
+        self.gameType = gameType
+    }
+    
+    func getUser() -> Player {
+        return self.user
+    }
+    
+    func getComputer() -> Player {
+        return self.computer
+    }
+    
+    func getTurn() -> Player {
+        return self.turn
+    }
+    
+    func setTurn(to player: Player) {
+        self.turn = player
+    }
+    
+    func playRockScissorPaper(userInput: Int) {
+        user.setRockScissorPaper(input: userInput)
+        computer.setRockScissorPaper(input: Int.random(in: 1...3))
+    }
+    
+    func playMookJjiBba(userInput: Int) {
+        user.setMookJjiBba(input: userInput)
+        computer.setMookJjiBba(input: Int.random(in: 1...3))
+    }
+    
+    func evaluateRockScissorPaper() {
+        if self.user.getRockScissorPaper() == self.computer.getRockScissorPaper() {
+            print(GameResult.draw.rawValue)
+            return
+        }
+        
+        switch user.getRockScissorPaper() {
+        case .rock:
+            self.turn = computer.getRockScissorPaper() == .scissor ? user : computer
+        case .scissor:
+            self.turn = computer.getRockScissorPaper() == .paper ? user : computer
+        case .paper:
+            self.turn = computer.getRockScissorPaper() == .rock ? user : computer
+        }
+        
+        if self.turn.getName() == "사용자" {
+            print(GameResult.win.rawValue)
+        } else {
+            print(GameResult.lose.rawValue)
+        }
+        self.gameType = .mookJjiBba
+    }
+
+    func evaluateMookJjiBba() -> GameResult {
+        if self.user.getMookJjiBba() == self.computer.getMookJjiBba() {
+            print("\(self.getTurn().getName())의 승리!")
+            return .win
+        }
+        
+        switch user.getMookJjiBba() {
+        case .rock:
+            self.turn = computer.getMookJjiBba() == .scissor ? user : computer
+        case .scissor:
+            self.turn = computer.getMookJjiBba() == .paper ? user : computer
+        case .paper:
+            self.turn = computer.getMookJjiBba() == .rock ? user : computer
+        }
+        print("\(self.getTurn().getName())의 턴입니다.")
+        return .draw
+    }
+}
