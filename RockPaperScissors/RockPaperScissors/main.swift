f5cb9ffd357890ab6982384cd0ddd8c69b8e5e7a
eensungkim <eensungkim@gmail.com>
2023-11-28 11:52:00 +0900

    refactor: 메인 로직 GameMaster의 playGame 메서드로 호출하도록 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -13,39 +13,7 @@ func main() {
     let gameType = GameType.rockScissorPaper
     var gameMaster = GameMaster(gameType: gameType, user: user, computer: computer, turn: computer)
 
-    var userInput: String?
     
-    gameLoop : repeat {
-        print(gameType.message(gameType: gameMaster.getGameType(), turn: gameMaster.getTurn()), terminator: "")
-        userInput = readLine()
-        
-        switch userInput {
-        case "0":
-            break gameLoop
-        case "1", "2", "3":
-            guard let input = userInput, let intUserInput = Int(input) else {
-                break
-            }
-            
-            switch gameMaster.getGameType() {
-            case .rockScissorPaper:
-                gameMaster.playRockScissorPaper(userInput: intUserInput)
-                gameMaster.evaluateRockScissorPaper()
-            
-            case .mookJjiBba:
-                gameMaster.playMookJjiBba(userInput: intUserInput)
-                let result = gameMaster.evaluateMookJjiBba()
-                if result == .win {
-                    break gameLoop
-                }
-            }
-        default :
-            gameMaster.setTurn(to: computer)
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-        }
-    } while userInput != "0"
-    
-    print("게임 종료")
 }
 
 
