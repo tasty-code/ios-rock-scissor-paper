ecf97b9d587e56577f1c251d8681a696fee93044
eensungkim <eensungkim@gmail.com>
2023-11-29 13:15:01 +0900

    refactor : 묵찌빠 로직 구조체로 분리

--- RockPaperScissors/RockPaperScissors/structures/MookJjiBba.swift
+++ RockPaperScissors/RockPaperScissors/structures/MookJjiBba.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct MookJjiBba {
+    func play(user: Player, computer: Player, turn: Turn, userInput: Int) -> GameResult {
+        var user = user
+        var computer = computer
+        var turn = turn
+        
+        user.chooseMookJjiBba(input: userInput)
+        computer.chooseMookJjiBba(input: Int.random(in: 1...3))
+        
+        let userHand = user.retrieveMookJjiBba()
+        let computerHand = computer.retrieveMookJjiBba()
+        
+        if userHand == computerHand {
+            print("\(turn.rawValue)의 승리!")
+            return .win
+        }
+        
+        switch userHand {
+        case .mook:
+            turn = computerHand == .jji ? .user : .computer
+        case .jji:
+            turn = computerHand == .bba ? .user : .computer
+        case .bba:
+            turn = computerHand == .mook ? .user : .computer
+        }
+        print("\(turn.rawValue)의 턴입니다.")
+        return .draw
+    }
+}
