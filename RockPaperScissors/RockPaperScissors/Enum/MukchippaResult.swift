9c436aaa0ae36c495940adc10b0cd6bcf215207e
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-11-29 10:02:33 +0900

    feat: 묵찌빠 결과 열거형 생성

---
+++ RockPaperScissors/RockPaperScissors/Enum/MukchippaResult.swift
@@ -0,0 +1,14 @@
+//
+//  MukchippaResult.swift
+//  RockPaperScissors
+//
+//  Created by nayeon  on 2023/11/29.
+//
+
+import Foundation
+
+enum MukchippaResult: Int {
+    case draw = 0
+    case computerWin = 1
+    case computerLose = 2
+}
