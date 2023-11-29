e7938b3acb28b7b988c1322a4782fd0808d2729b
eunjuchoi <eunjuzzoo@naver.com>
2023-11-29 15:50:44 +0900

    refactor: Playable 프로토콜 수정 (#2)
    - associatedtype 정의
    - 프로토콜 익스텐션 활용한 메서드 기본 구현

--- RockPaperScissors/RockPaperScissors/Protocol/Playable.swift
+++ RockPaperScissors/RockPaperScissors/Protocol/Playable.swift
@@ -8,9 +8,28 @@
 import Foundation
 
 protocol Playable {
+    associatedtype GameType
     
-    func validateUserInput(_ input: String?) throws -> RockPaperScissorsType
-    func judgeGame(user: RockPaperScissorsType, computer: RockPaperScissorsType?, turn: PlayerType) -> GameResultType
+    func validateUserInput(_ input: String?) throws -> Int
+    func judgeGame(user: GameType?, computer: GameType?, turn: PlayerType) -> GameResultType
     func showMessage(_ messageType: GameResultType, _ turn: PlayerType)
     func playGame(_ result: GameResultType) -> GameResultType
+    func judgeWinner(_ gameResult: GameResultType) -> PlayerType
+}
+
+extension Playable {
+    
+    func validateUserInput(_ input: String?) throws -> Int {
+        guard let input = input,
+              let num = Int(input),
+              (0...3).contains(num) else {
+            throw GameError.invalidInput
+        }
+        
+        return num
+    }
+    
+    func judgeWinner(_ gameResult: GameResultType) -> PlayerType {
+        return gameResult == .win ? .user : .computer
+    }
 }
