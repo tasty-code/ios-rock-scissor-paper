1637ed71f47591ed346303295a750104b9e89a72
eunjuchoi <eunjuzzoo@naver.com>
2023-11-29 15:52:44 +0900

    refactor: 가위바위보, 묵찌빠 타입 관련 리팩토링 (#2)
    - 묵찌빠 enum 타입 추가
    - Convertible 프로토콜 정의

--- RockPaperScissors/RockPaperScissors/Enum/RockPaperScissorsType.swift
+++ RockPaperScissors/RockPaperScissors/Enum/RockPaperScissorsType.swift
@@ -7,23 +7,9 @@
 
 import Foundation
 
-@frozen
 enum RockPaperScissorsType: Int {
     case none = 0
     case scissors = 1
     case rock = 2
     case paper = 3
-    
-    func getAdvancedType() -> RockPaperScissorsType {
-        switch self {
-        case .none:
-            return .none
-        case .scissors:
-            return .rock
-        case .rock:
-            return .scissors
-        case .paper:
-            return .paper
-        }
-    }
 }
