d200024dc55c4184782cf83ce4aad119bb7dbb6a
Paul Lee <earths-voluble.0g@icloud.com>
2023-11-29 13:44:00 +0900

    🏗️ [architecture] Player, Refree 객체 추가 및 구조 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -13,6 +13,53 @@ enum RockScissorsPaper: String, CaseIterable {
     case paper = "3"
 }
 
+enum MukJiPa: String, CaseIterable {
+    case exit = "0"
+    case muk = "1"
+    case ji = "2"
+    case pa = "3"
+}
+
+enum Result: String {
+    case exit, win, draw, lose
+}
+
+struct Player {
+    var rockScissorsPaperHand: RockScissorsPaper?
+    
+    mutating func choiceRockScissorsPaperHand() {
+        if let input = readLine(),
+           let select = RockScissorsPaper(rawValue: input) {
+            let playerHand = select
+            self.rockScissorsPaperHand = playerHand
+        }
+    }
+    
+    mutating func randomRockScissorsPaperHand() {
+        let exceptExitCase = RockScissorsPaper.allCases.filter{ $0.rawValue != "0" }
+        self.rockScissorsPaperHand = exceptExitCase.randomElement()
+    }
+    
+}
+
+struct Refree {
+    func runRockScissorsPaper(playerHand: RockScissorsPaper?, computerHand: RockScissorsPaper?) -> Result {
+        if playerHand == .exit {
+            return Result.exit
+        } else {
+            if playerHand == computerHand {
+                return Result.draw
+            } else if (playerHand == .rock && computerHand == .scissors) ||
+                        (playerHand == .scissors && computerHand == .paper) ||
+                        (playerHand == .paper && computerHand == .rock) {
+                return Result.win
+            } else {
+                return Result.lose
+            }
+        }
+    }
+}
+
 func RockScissorsPaperGame(_ userChoice: RockScissorsPaper) -> Bool {
     let userHand = Int(userChoice.rawValue)
     let computerHand = Int.random(in: 1...3)
@@ -38,18 +85,29 @@ func endGame() {
     onGame.toggle()
 }
 
-func launchGame(_ input: RockScissorsPaper) {
-    if RockScissorsPaperGame(input) {
+func startGame() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> :")
+    let refree = Refree()
+    var user = Player()
+    var computer = Player()
+    
+    user.choiceRockScissorsPaperHand()
+    computer.randomRockScissorsPaperHand()
+    
+    let rockScissorPaperResult = refree.runRockScissorsPaper(playerHand: user.rockScissorsPaperHand,
+                                                             computerHand: computer.rockScissorsPaperHand)
+    switch rockScissorPaperResult {
+    case .exit:
         endGame()
+    case .draw:
+        print("비겼습니다!")
+        startGame()
+        return
+    default:
+        print("승리/패배-묵찌빠로")
     }
 }
 
 while onGame {
-    print("가위(1), 바위(2), 보(3)! <종료 : 0> :")
-    if let input = readLine(),
-       let select = RockScissorsPaper(rawValue: input) {
-        launchGame(select)
-    } else {
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-    }
+    startGame()
 }
