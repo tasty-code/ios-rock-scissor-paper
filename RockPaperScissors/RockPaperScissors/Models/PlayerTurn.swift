bc84f83e90ed21f56711267734fabb178d8cb75f
ijs1103 <ajaijs1103@naver.com>
2023-11-29 10:34:02 +0900

    feat(PlayerTurn): PlayerTurn 모델 정의

---
+++ RockPaperScissors/RockPaperScissors/Models/PlayerTurn.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerTurn.swift
+//  RockPaperScissors
+//
+//  Created by 이주상 on 2023/11/29.
+//
+
+import Foundation
+
+enum PlayerTurn: String {
+    case human = "사용자"
+    case computer = "컴퓨터"
+}
