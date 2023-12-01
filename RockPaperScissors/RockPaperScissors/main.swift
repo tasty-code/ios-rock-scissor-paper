6e151666de471172b795f39f45516b3ad1bbfafd
Hoi.Kwon <teaho177@gmail.com>
2023-12-01 14:25:22 +0900

    refactor: randomComputerPick 줄바꿈

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,7 +4,9 @@ var  isGameRunning: Bool = true
 
 while isGameRunning {
     GameIOManager.showGameMenu(for: .menu)
-    guard let randomComputerPick = RockScissorPaperUserOption.allCases.filter({ $0 != .exit }).randomElement() else { continue }
+    guard let randomComputerPick =
+            RockScissorPaperUserOption.allCases.filter({ $0 != .exit }).randomElement()
+    else { continue }
     guard
         let input = readLine(),
         let userInput = Int(input),
