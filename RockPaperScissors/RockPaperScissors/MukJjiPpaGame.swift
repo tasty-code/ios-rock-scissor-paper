8b6d668619e66944958272d0306b750b8f1e3457
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-01 17:14:01 +0900

    feat: rockScissorPaperGame의 결과값으로 게임 턴 설정 및 게임 진행 로직 구현

--- RockPaperScissors/RockPaperScissors/MukJjiPpaGame.swift
+++ RockPaperScissors/RockPaperScissors/MukJjiPpaGame.swift
@@ -7,15 +7,21 @@
 
 struct MukJjiPpaGame {
     private var rockScissorPaperGame: RockScissorPaperGame
+    private var turn: Turn?
     
     init(rockScissorPaperGame: RockScissorPaperGame) {
         self.rockScissorPaperGame = rockScissorPaperGame
     }
     
     mutating func play() {
-        rockScissorPaperGame.play()
+        guard
+            let result = rockScissorPaperGame.play()
+        else {
+            return
+        }
         
-        while rockScissorPaperGame.isRunning {
+        turn = result == .win ? .user : .computer
+        while true {
             guard
                 continueGame()
             else {
@@ -25,23 +31,23 @@ struct MukJjiPpaGame {
     }
     
     private mutating func continueGame() -> Bool {
-        print("\(rockScissorPaperGame.winner ?? .user) 턴 입니다.")
-        print("[\(rockScissorPaperGame.winner ?? .user) 턴] 묵(1), 찌(2), 빠(3)! <종료: 0> :", terminator: " ")
+        guard let turn = turn else { return false }
+        print("\(turn) 턴 입니다.")
+        print("[\(turn) 턴] 묵(1), 찌(2), 빠(3)! <종료: 0> :", terminator: " ")
         
         let userInput = readLine()
         
         guard
             let command = Command(value: userInput)
         else {
-            rockScissorPaperGame.winner = .computer
-            print(ApplicationStatusMessage.error)
+            invalidCommandEntered()
             return true
         }
         
         guard
             !command.isQuit
         else {
-            print(ApplicationStatusMessage.quit)
+            print(GameGuideMessage.quit)
             return false
         }
         
@@ -58,14 +64,20 @@ struct MukJjiPpaGame {
     private mutating func compareChoice(user: MukJjiPpaChoice, computer: MukJjiPpaChoice) -> Bool {
         switch (user, computer) {
         case (.jji, .muk),(.muk, .ppa),(.ppa, .jji):
-            rockScissorPaperGame.winner = .computer
+            turn = .computer
             return true
         case (.jji, .ppa),(.muk, .jji),(.ppa, .muk):
-            rockScissorPaperGame.winner = .user
+            turn = .user
             return true
         default:
-            print("\(rockScissorPaperGame.winner ?? .user)의 승리!")
+            guard let turn = turn else { return false }
+            print("\(turn)의 승리!")
             return false
         }
+    }
+    
+    private mutating func invalidCommandEntered() {
+        turn = .computer
+        print(GameGuideMessage.error)
     }
 }
